0dd3793af1458aeb2012fc44ad18ee049eaca2c0
Pavlo Fedas <pavlofedas25@gmail.com>
2025-10-03 20:50:22 +0300

    2hw3

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import type { AxiosResponse } from 'axios';
 import type { Movie } from '../types/movie';
 
 interface TMDBResponse {
@@ -9,31 +8,18 @@ interface TMDBResponse {
   total_pages: number;
 }
 
-const TMDB_TOKEN = import.meta.env.VITE_TMDB_TOKEN as string | undefined;
-
-if (!TMDB_TOKEN) {
-  console.warn('VITE_TMDB_TOKEN is not set. Add it to .env');
-}
+const TMDB_TOKEN = import.meta.env.VITE_TMDB_TOKEN as string;
 
 const axiosInstance = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
   headers: {
-    Authorization: `Bearer ${TMDB_TOKEN ?? ''}`,
+    Authorization: `Bearer ${TMDB_TOKEN}`,
     Accept: 'application/json',
   },
 });
 
-export async function fetchMovies(query: string, page = 1): Promise<TMDBResponse> {
-  const params = {
-    query,
-    include_adult: false,
-    page,
-    language: 'en-US',
-  };
-
-  const response: AxiosResponse<TMDBResponse> = await axiosInstance.get('/search/movie', {
-    params,
-  });
-
+export async function fetchMovies(query: string) {
+  const params = { query, include_adult: false, language: 'en-US' };
+  const response = await axiosInstance.get<TMDBResponse>('/search/movie', { params });
   return response.data;
 }
